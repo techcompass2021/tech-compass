e370f732a176e4aff4ceaa4fdf854488b2c4fae1
Kotomi Obata <k.obata822@gmail.com>
2021-06-22 17:25:49 +0900

    課題提出 PHP-データ型

---
+++ dataType/taskDataType.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskDataType</title>
+</head>
+<body>
+
+    <!-- 課題1　-->
+    <p><?php
+    $task1 = 5;
+    echo("りんごが" . "$task1" . "個あります。");
+    ?></p>
+   
+    <!-- 課題2　-->
+    <p><?php echo("string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\)が必要です。"); ?></p>
+
+    <!-- 課題3　-->
+    <p><?php echo('I\'m Security Engineer'); ?></p>
+
+    <!-- 課題3　-->
+    <p><?php
+        const GOOD_EVENING = "こんばんは";
+        $name = "太郎くん";
+        echo(GOOD_EVENING . "、$name");
+    ?></p>
+   
+</body>
+</html>
+
