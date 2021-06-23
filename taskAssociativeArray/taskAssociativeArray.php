2400084a1edfbbda0d4522c8acdefd77ad512f03
Kotomi Obata <k.obata822@gmail.com>
2021-06-23 23:34:40 +0900

    課題提出 PHP-連想配列

--- taskAssociativeArray/taskAssociativeArray.php
+++ taskAssociativeArray/taskAssociativeArray.php
@@ -8,6 +8,61 @@
 </head>
 <body>
 
+<?php 
+    $testScoreArray = [
+        'A' => [
+           '国語' => 85,
+           '数学' => 64,
+           '社会' => 45,
+           '理科' => 77,
+           '英語' => 80, 
+        ],
+        'B' => [
+            '国語' => 56,
+            '数学' => 89,
+            '社会' => 73,
+            '理科' => 85,
+            '英語' => 78, 
+        ],
+        'C' => [
+            '国語' => 98,
+            '数学' => 87,
+            '社会' => 88,
+            '理科' => 92,
+            '英語' => 96, 
+        ],
+    ];
+    ?>
+
+    <!-- 課題1-1 -->
+    <?php $scienceA = $testScoreArray['A']['理科']; ?>
+    <p>1: <?php echo($scienceA); ?>点</p>
+
+    <!-- 課題1-2 -->
+    <?php $englishB = $testScoreArray['B']['英語']; ?>
+    <p>2: <?php echo($englishB); ?>点</p>
+
+    <!-- 課題1-3 -->
+    <?php  
+        $sumA = array_sum($testScoreArray['A']); 
+        $averageA = round($sumA / count($testScoreArray['A']));
+    ?>
+    <p>3: <?php echo($averageA); ?>点</p>
+
+    <!-- 課題1-4 -->
+    <?php 
+        $sumC = array_sum($testScoreArray['B']);
+        $averageC = round($sumC / count($testScoreArray['C']));
+    ?>
+    <p>4: <?php echo($averageC); ?>点</p>
+
+    <!-- 課題1-5 -->
+    <?php
+        $sumMath = array_sum(array_column($testScoreArray,'数学'));
+        $averageMath = round($sumMath / count($testScoreArray));
+    ?>
+    <p>5: <?php echo($averageMath); ?>点</p>
+
 
 </body>
 </html>
