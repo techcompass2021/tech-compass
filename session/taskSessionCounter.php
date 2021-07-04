a6147366e62f42d2427477734cdef6da61ea0d0b
Kotomi Obata <k.obata822@gmail.com>
2021-07-04 23:24:31 +0900

    test

---
+++ session/taskSessionCounter.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * 表示を行う
+ * 
+ * @param  string $string
+ * @return null
+ */
+function display(string $string)
+{
+    echo ('<p>' . $string . '</p>');
+}
+
+// ここに回答を追記する
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>taskSessionCounter</title>
+    </head>
+    <body>
+        <?php display( '回目のアクセスです。'); ?>
+        <button onclick="location.href='taskSessionCounter.php?clear=0'">
+            更新
+        </button>
+        <button onclick="location.href='taskSessionCounter.php?clear=1'">
+            クリア
+        </button>
+    </body>
+</html>
