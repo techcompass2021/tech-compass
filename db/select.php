fc3326a65be98f4019f4a44bf7f4bab5cecdf068
小幡琴美 <k.obata822@gmail.com>
2021-07-06 13:50:23 +0900

    課題提出 PHP-データ検索

--- db/select.php
+++ db/select.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>テーブル作成（CREATE）</title>
+        <title>データ検索（SELECT）</title>
     </head>
     <body>
         <h1>課題1</h1>
