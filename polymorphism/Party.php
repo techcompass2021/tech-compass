3f995b2c45f530142f47631fdd6963cebb6a12ec
kotomi obata <k.obata822@gmail.com>
2021-07-14 10:45:33 +0900

    再提出 PHP-ポリモーフィズム

--- polymorphism/Party.php
+++ polymorphism/Party.php
@@ -7,16 +7,15 @@ class Party {
     public function __construct(Hero $hero) {
         //クラス変数パーティに追加
         $this->partyArray[$hero->name] = $hero;
-        //$this->attack = $hero->attack();
     }
 
     public function addHero(Hero $hero) : string {
-        $this->name = $hero->name;
+        $name = $hero->name;
         if(count($this->partyArray) >= self::PARTY_MAX) {
             $addHeroMessage = "パーティーの上限数に達しています。";
         } else {
-            $this->partyArray[$this->name] = $hero;
-            $addHeroMessage = $this->name . "が仲間になりました!";
+            $this->partyArray[$name] = $hero;
+            $addHeroMessage = $name . "が仲間になりました!";
         }
         
         return $addHeroMessage;
@@ -25,8 +24,8 @@ class Party {
     public function allHeroAttack () {
         //クラス変数パーティが全員攻撃 
         foreach($this->partyArray as $key => $value) {
-                echo  "<p>" . $key . "のターンです。</p>";
-                echo  "<p>" . $value->attack() . "</p>";
+            echo  "<p>" . $key . "のターンです。</p>";
+            echo  "<p>" . $value->attack() . "</p>";
         }
     }
 
