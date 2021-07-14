51628f96c6e7bfd34600f8a6c54f5e3053aa1ee6
kotomi obata <k.obata822@gmail.com>
2021-07-14 10:47:14 +0900

    再提出 PHP-抽象

--- abstract/Party.php
+++ abstract/Party.php
@@ -7,19 +7,18 @@ class Party {
     public function __construct(Hero $hero) {
         //クラス変数パーティに追加
         $this->partyArray[$hero->name] = $hero;
-        //$this->attack = $hero->attack();
     }
 
     public function addHero(Hero $hero) : string {
-        $this->name = $hero->name;
+        $name = $hero->name;
         
         if (in_array($hero, $this->partyArray )){
-            $addHeroMessage = $this->name . "はすでに仲間にいます。";
+            $addHeroMessage = $name . "はすでに仲間にいます。";
         } elseif (count($this->partyArray) >= self::PARTY_MAX) {
             $addHeroMessage = "パーティーの上限数に達しています。";
         } else {
-            $this->partyArray[$this->name] = $hero;
-            $addHeroMessage = $this->name . "が仲間になりました!";
+            $this->partyArray[$name] = $hero;
+            $addHeroMessage = $name . "が仲間になりました!";
         }
         
         return $addHeroMessage;
@@ -28,8 +27,8 @@ class Party {
     public function allHeroAttack () {
         //クラス変数パーティが全員攻撃 
         foreach($this->partyArray as $key => $value) {
-                echo  "<p>" . $key . "のターンです。</p>";
-                echo  "<p>" . $value->attack() . "</p>";
+            echo  "<p>" . $key . "のターンです。</p>";
+            echo  "<p>" . $value->attack() . "</p>";
         }
         
     }
